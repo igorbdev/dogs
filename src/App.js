c81f338d036cf1d43bdea101dcaafec4289839e1
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-07-25 11:10:42 -0300

    init react dogs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Api from "./api/Api";
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
