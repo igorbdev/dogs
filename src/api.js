326f729bcfe0e93124356324ab6ebe6f0fd01d34
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-07-30 09:37:24 -0300

    add cadastro

--- src/api.js
+++ src/api.js
@@ -39,3 +39,17 @@ export function USER_GET(token) {
     },
   };
 }
+
+// Função para criar usuario
+export function USER_POST(body) {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
