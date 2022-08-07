382e8a8fab5e4277a49875bdb08fe9c3c3bc5396
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-08-07 10:31:40 -0300

    add Erro and PropTypes

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import FeedPhotosItem from './FeedPhotosItem';
-import useFetch from '../../Hooks/useFetch';
-import { PHOTOS_GET } from '../../Api';
-import Error from '../Helper/Error';
-import Loading from '../Helper/Loading';
-import styles from './FeedPhotos.module.css';
+import React from "react";
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../Api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import styles from "./FeedPhotos.module.css";
 
 const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
@@ -14,7 +14,6 @@ const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
       const total = 6;
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
-      console.log('Request:', json);
       if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
