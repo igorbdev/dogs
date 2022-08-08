ce7ace7f8362f2e0846b275a4534cab720b57c9a
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-08-08 10:35:52 -0300

    heade and footer

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import useFetch from '../../Hooks/useFetch';
-import { PHOTO_GET } from '../../Api';
-import Error from '../Helper/Error';
-import Loading from '../Helper/Loading';
-import PhotoContent from './PhotoContent';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../Api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import PhotoContent from "./PhotoContent";
+import Head from "../Helper/Head";
 
 const Photo = () => {
   const { id } = useParams();
@@ -20,6 +21,7 @@ const Photo = () => {
   if (data)
     return (
       <section className="container mainContainer">
+        <Head title={data.photo.title} />
         <PhotoContent single={true} data={data} />
       </section>
     );
