6f3fd638c84d0e4746cfee6a4d705841b4b69bff
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-08-29 17:06:03 -0300

    userLogin

---
+++ src/store/user.js
@@ -0,0 +1,15 @@
+import createAsyncSlice from "./helper/createAsyncSlice";
+import { USER_GET } from "../Api";
+import { fetchToken } from "./token";
+
+const slice = createAsyncSlice({
+  name: "user",
+  fetchConfig: (token) => USER_GET(token),
+});
+
+export const fetchUser = slice.asyncAction;
+export const userLogin = (user) => async (dispatch) => {
+  const { payload } = await dispatch(fetchToken(user));
+  if (payload.token) await dispatch(fetchUser(payload.token));
+};
+export default slice.reducer;
